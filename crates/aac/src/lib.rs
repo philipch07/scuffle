0737aa6f05890fe49fdb0cd3615f9dba992fecab
Lennart Kloock <lennart.kloock@protonmail.com>
2025-01-10 18:18:17 +0100

    chore(aac): fmt

--- crates/aac/src/lib.rs
+++ crates/aac/src/lib.rs
@@ -4,8 +4,9 @@
 //!
 //! ## License
 //!
-//! This project is licensed under the [MIT](./LICENSE.MIT) or [Apache-2.0](./LICENSE.Apache-2.0) license.
-//! You can choose between one of them if you use this work.
+//! This project is licensed under the [MIT](./LICENSE.MIT) or
+//! [Apache-2.0](./LICENSE.Apache-2.0) license. You can choose between one of
+//! them if you use this work.
 //!
 //! `SPDX-License-Identifier: MIT OR Apache-2.0`
 #![cfg_attr(all(coverage_nightly, test), feature(coverage_attribute))]
