1514c7e89a765c65765845d26f59cf93d112ae91
Lennart Kloock <lennart.kloock@protonmail.com>
2025-01-13 19:40:03 +0100

    docs(metrics): root docs

--- crates/metrics/src/lib.rs
+++ crates/metrics/src/lib.rs
@@ -1,3 +1,20 @@
+//! # scuffle-metrics
+//!
+//! A wrapper around opentelemetry to provide a more ergonomic interface for creating metrics.
+//!
+//! ## Status
+//!
+//! This crate is currently under development and is not yet stable.
+//!
+//! Unit tests are not yet fully implemented. Use at your own risk.
+//!
+//! ## License
+//!
+//! This project is licensed under the [MIT](./LICENSE.MIT) or [Apache-2.0](./LICENSE.Apache-2.0) license.
+//! You can choose between one of them if you use this work.
+//!
+//! `SPDX-License-Identifier: MIT OR Apache-2.0`
+
 #[cfg(feature = "prometheus")]
 /// A copy of the opentelemetry-prometheus crate, updated to work with the
 /// latest version of opentelemetry.
