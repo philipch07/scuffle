16e80aa65c569d6b6013509a66333c6431e5f420
Lennart Kloock <lennart@scuffle.cloud>
2025-01-19 00:39:25 +0100

    fix(ci): add pipefail stuff to bash script

--- .github/patch-docs.sh
+++ .github/patch-docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 if [ -n "$4" ]
     then
         sed -i "s#</nav><div class=\"sidebar-resizer\"#<div class=\"version\">Deployed from<br><a href=\"$1/pull/$4\">Pull Request $4</a><br><a href=\"$1/commit/$2\">Commit <code>$3</code></a></div></nav><div class=\"sidebar-resizer\"#" target/doc/index.html
